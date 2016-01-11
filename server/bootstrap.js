e2dc9cef8879099a6c8921a4655f3d912a2dbb98
Piyush Goyal <piyush.goyal@scalearc.com>
2016-01-11 15:11:00 +0530

    config changes

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -23,71 +23,7 @@ Meteor.startup(function() {
                     TotalConnections:"",
                     Concurrency:"",
                     AbsoluteFilePath:""
-                },
-                createdBy:"qFJdcSHb5Rcuv2F3r"
-            },
-            {
-                ConfigName:"CONFIG 1",
-                ConfType:"DbServer",
-                IP:"10.11.0.72",
-                User:"root",
-                pass:"r00t.!@#",
-                ScriptPath:"/root/client",
-                ScriptName: "master.sh",
-                config: {
-                    ServerName:"10.27.0.93",
-                    ServerPort:"11106",
-                    UserName:"system",
-                    Password:"Info_123",
-                    DatabaseName:"orcl",
-                    ClientVersion:"1",
-                    TotalConnections:"1",
-                    Concurrency:"1",
-                    AbsoluteFilePath:"/home/idb/javaClient/testcase.sql"
-                },
-                createdBy:"qFJdcSHb5Rcuv2F3r"
-            },
-            {
-                ConfigName:"CONFIG 2",
-                ConfType:"DbServer",
-                IP:"10.11.0.227",
-                User:"root",
-                pass:"r00t.!@#",
-                ScriptPath:"/root/client",
-                ScriptName: "master.sh",
-                config: {
-                    ServerName:"10.27.0.93",
-                    ServerPort:"11106",
-                    UserName:"system",
-                    Password:"Info_123",
-                    DatabaseName:"orcl",
-                    ClientVersion:"2",
-                    TotalConnections:"1",
-                    Concurrency:"1",
-                    AbsoluteFilePath:"/home/idb/javaClient/testcase.sql"
-                },
-                createdBy:"qFJdcSHb5Rcuv2F3r"
-            },
-            {
-                ConfigName: "CONFIG 3",
-                ConfType:"DbServer",
-                IP: "10.0.53.10",
-                User: "idb",
-                pass: "r00t.!@#",
-                ScriptPath: "",
-                ScriptName: "./testScript.sh",
-                config: {
-                    ServerName: "10.27.0.93",
-                    ServerPort: "11106",
-                    UserName: "system",
-                    Password: "Info_123",
-                    DatabaseName: "orcl",
-                    ClientVersion: "2",
-                    TotalConnections: "1",
-                    Concurrency: "1",
-                    AbsoluteFilePath: "/home/idb/javaClient/testcase.sql"
-                },
-                createdBy:"qFJdcSHb5Rcuv2F3r"
+                }
             }
         ];
 
